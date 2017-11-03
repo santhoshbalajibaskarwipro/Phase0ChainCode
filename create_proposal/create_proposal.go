3e4ac08c5103f11d4bd8a3641ba93277db4b1923
abhi-005 <31097549+abhi-005@users.noreply.github.com>
2017-11-03 15:06:58 +0530

    Update create_proposal.go

--- create_proposal/create_proposal.go
+++ create_proposal/create_proposal.go
@@ -1,21 +1,7 @@
-/*/*
--a-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license Forms.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0 
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
+/*****Project ChainDaaS - Phase0 - SRC - UI1 for Proposal Entry ***/
+/*** Last updated on 3rd Nov 2017 by Abhishek Kumar ***/
+/*** github link :: https://github.com/abhi-005/Phase0ChainCode/create_proposal/ ***/
+/*** File name create_proposal.go ***/
 
 package main
 
@@ -36,7 +22,7 @@ type ManageProposal struct {
 var approved_proposal_entry = "approved_proposal_entry"				//name for the key/value that will store a list of all known  Tier3 Form
 
 type proposal struct{
-								// Attributes of a Form 
+								
 	proposal_id string `json:"proposal_id"`	
 	region string `json:"region"`
 	country string `json:"country"`
@@ -58,7 +44,7 @@ type proposal struct{
 	
 }
 // ============================================================================================================================
-// Main - start the chaincode for Form management
+// Main - start the chaincode for Proposal Form management
 // ============================================================================================================================
 func main() {			
 	err := shim.Start(new(ManageProposal))
@@ -91,10 +77,7 @@ func (t *ManageProposal) Init(stub shim.ChaincodeStubInterface, function string,
 		return nil, err
 	}
 	return nil, nil
-}
-// ============================================================================================================================
-// Run - Our entry Formint for Invocations - [LEGACY] obc-peer 4/25/2016
-// ============================================================================================================================
+} 
 func (t *ManageProposal) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("run is running " + function)
 	return t.Invoke(stub, function, args)
