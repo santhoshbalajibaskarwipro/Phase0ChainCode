76e68c5f92b67e3b1fcdd01b85670ae1ef075347
abhi-005 <31097549+abhi-005@users.noreply.github.com>
2017-11-03 16:10:27 +0530

    Create create_purchase_order.go

---
+++ create_purchase_order/create_purchase_order.go
@@ -0,0 +1,4 @@
+/***** Project ChainDaaS - Phase0 - SRC - UI1 for Purchase Order ***/
+/*** Last updated on 3rd Nov 2017 by Abhishek Kumar ***/
+/*** github link :: https://github.com/abhi-005/Phase0ChainCode/create_purchase_order/ ***/
+/*** File name create_purchase_order.go ***/
